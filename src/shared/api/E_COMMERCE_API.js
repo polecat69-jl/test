ec691ce5e606239271070b0de1d3fb7b344332cb
polecat69-jl <info@jldigital.se>
2021-05-25 16:53:43 +0200

    production

--- src/shared/api/E_COMMERCE_API.js
+++ src/shared/api/E_COMMERCE_API.js
@@ -1,10 +1,10 @@
 import Axios from 'axios'
 
-const productionAPI = 'https://'
+const productionAPI = 'https://calm-reef-42496.herokuapp.com/'
 const developmentAPI = 'http://localhost:3001'
 
 const E_COMMERCE_API = Axios.create({
-    baseURL: developmentAPI
+    baseURL: productionAPI
 })
 
 export default E_COMMERCE_API
